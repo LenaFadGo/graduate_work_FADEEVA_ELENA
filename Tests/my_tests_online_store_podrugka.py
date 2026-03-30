3cf391d5312cdfb6ad090f73cb3b80e0cb3607a0
RERIK <glebkrot200800@gmail.com>
2026-03-30 21:18:28 +0500

    Автотест 30.03.2026

--- Tests/my_tests_online_store_podrugka.py
+++ Tests/my_tests_online_store_podrugka.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
+from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 10)
 
-def test_open_podrygka():
-    driver.get("https://www.podrygka.ru/")
-    accept_button = wait.until(EC.visibility_of_element_located((By.XPATH, "//span[text()='Подарочные карты']")))
+def test_open_podrygka(driver_init):
+    driver_init.get("https://www.podrygka.ru/")
+    wait = WebDriverWait(driver_init, 15)
+
+    accept_button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "a.button.button--default.button--default-round")))
     accept_button.click()
+
+    sleep(2)
+    search = wait.until(EC.element_to_be_clickable((By.ID, "search")))
+    search.click()
+    search.send_keys("essence")
+    button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.digi-ac-find__button')))
+    button.click()

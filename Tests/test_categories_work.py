02c40596fc4607cf4e9788d0f6d646ad334c6708
FadeevaQA <hohoho_lena@mail.ru>
2026-04-16 17:44:45 +0500

    Продолжение дипломной работы 02.04.2026

--- Tests/test_categories_work.py
+++ Tests/test_categories_work.py
@@ -1,22 +1,29 @@
-import selenium
+import allure
 from time import sleep
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+
+@allure.epic("E-commerce Tests")
+@allure.feature("Catalog Navigation")
+@allure.story("Korean Cosmetics Category")
+@allure.title("Переход в категорию 'Корея' через каталог")
 def test_open_podrygka(driver_init):
-    driver_init.get("https://www.podrygka.ru/")
-    wait = WebDriverWait(driver_init, 10)
+    with allure.step("Шаг 1: Открытие главной страницы"):
+        driver_init.get("https://www.podrygka.ru/")
+        wait = WebDriverWait(driver_init, 10)
 
-    accept_button = wait.until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "a.button.button--default.button--default-round")))
-    accept_button.click()
+    with allure.step("Шаг 2: Закрытие всплывающего окна"):
+        accept_button = wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, "a.button.button--default.button--default-round")))
+        accept_button.click()
 
-    sleep(10)
-    catalog_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.header-menu-catalog')))
-    catalog_button.click()
+    with allure.step("Шаг 3: Открытие меню Каталога"):
+        sleep(5)
+        catalog_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.header-menu-catalog')))
+        catalog_button.click()
 
-    carusel = driver_init.find_element(By.XPATH, "//ul[@class = 'w-max flex gap-2.5 md:gap-1.5 xl:gap-2']")
-    category_for_men = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'a.relative.t3_s.flex-1.h-21.flex.justify-end.cursor-pointer')))
-    category_for_men.click()
-    pass
+    with allure.step("Шаг 4: Выбор категории 'Корея'"):
+        category_for_Koreya = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,'a.relative.t3_s.flex-1.h-21.flex.justify-end.cursor-pointer')))
+        category_for_Koreya.click()
